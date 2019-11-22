b1706dfab4457fba2d8c7ca5abf7ec99b9b97a7d
Gustavo Yepez <gustavo@Gustavos-MacBook-Pro.local>
2019-11-21 22:06:13 -0500

    done with part 1

--- src/components/NasaList.js
+++ src/components/NasaList.js
@@ -1,12 +1,12 @@
 import React, {useState, useEffect} from 'react';
-import ApodCard from './Photocard';
+import Photocard from './Photocard';
 import axios from 'axios';
 
 export default function NasaList() {
     const [pod, setPod] = useState([])
 
     useEffect(() => {
-        axios.get('https://api.nasa.gov/planetary/apod?api_key=ZwUKBPgwpfcXn99ZDah8BaLHsdzDA4djyPxwApVp')
+        axios.get('https://api.nasa.gov/planetary/apod?api_key=ZwUKBPgwpfcXn99ZDah8BaLHsdzDA4djyPxwApVp&date=2012-03-14')
         .then(response => {
             console.log(response.data)
             setPod(response.data)
